7cd4ee61382efedc33bce7ea6b0156f5ee667929
RuisongZhou <811437508@qq.com>
2020-05-05 09:59:25 +0800

    add distlinear

--- models/resnest/resnest.py
+++ models/resnest/resnest.py
@@ -142,7 +142,7 @@ class Resnest101(nn.Module):
         self.model = resnest101(pretrained=True)
         self.dropout = nn.Dropout(0.2)
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.last_linear = nn.Linear(512 * 4, num_classes)
+        self.last_linear = distLinear(512 * 4, num_classes)
         self.arc = ArcMarginProduct(2048, num_classes)
     def logits(self, x):
         x = self.avg_pool(x)
